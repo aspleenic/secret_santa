248db40f9b8e457a623ee311b436d168ed038101
PJ Hagerty <pjhagerty@gmail.com>
2012-11-09 16:22:57 -0500

    The repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecretSanta::Application.config.secret_token = '124e2f1cc4f3e0e24f459844a0fb21e1c7dde5d60599b1b2e47a444a6eec815cb21112d73aa1d86ae30b0817f311bf2df44a9e0b24d6aebd13861f5e10302afe'
